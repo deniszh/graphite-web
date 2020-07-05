5336eca3e277b5a6aeba920ec715217759df21bc
deniszh <denis.zhdanov@gmail.com>
2020-07-05 23:00:50 +0200

    Still not ready, but closer

--- webapp/graphite/render/hashing.py
+++ webapp/graphite/render/hashing.py
@@ -26,7 +26,7 @@ except ImportError:
 try:
     import pyhash
 
-    def fnv1a(data, seed=0x811c9dc5, mode=32, seed64=0xcbf29ce484222325):
+    def fnv1a(data, seed=2166136261, mode=32, seed64=14695981039346656037):
         hasher = pyhash.fnv1a_32()
         if mode > 32:
             hasher = pyhash.fnv1a_64()
@@ -34,16 +34,16 @@ try:
         return hasher(data, seed=seed)
 
 except ImportError:
-    def fnv1a(data, seed=0x811c9dc5, mode=32, seed64=0xcbf29ce484222325):
+    def fnv1a(data, seed=2166136261, mode=32, seed64=14695981039346656037):
         """
         FNV-1a Hash (http://isthe.com/chongo/tech/comp/fnv/) in Python.
         Taken from https://gist.github.com/vaiorabbit/5670985
         """
         hval = seed
-        fnv_prime = 0x01000193
+        fnv_prime = 16777619
         uint_max = 2 ** 32
         if mode > 32:
-            fnv_prime = 0x100000001b3
+            fnv_prime = 1099511628211
             uint_max = 2 ** 64
             hval = seed64
         if sys.version_info >= (3, 0):
@@ -58,6 +58,9 @@ except ImportError:
                 hval = (hval * fnv_prime) % uint_max
         return hval
 
+def fnv32a(data, seed=2166136261):
+    return fnv1a(data, seed, mode=32)
+
 try:
     import jump
 
@@ -68,9 +71,10 @@ except ImportError:
         """Fast, minimal memory, consistent hash algorithm.
         Taken from https://github.com/lithammer/python-jump-consistent-hash/blob/master/src/jump/__init__.py
         C version is 10 times faster, use jump module if possible
+        Accepts only integer as input
         """
         if sys.version_info >= (3, 0):
-            long = int
+            from builtins import int
         b, j = -1, 0
         if num_buckets < 1:
             raise ValueError(
@@ -78,7 +82,7 @@ except ImportError:
             )
         while j < num_buckets:
             b = int(j)
-            key = ((key * long(2862933555777941757)) + 1) & 0xFFFFFFFFFFFFFFFF
+            key = ((key * int(2862933555777941757)) + 1) & 0xFFFFFFFFFFFFFFFF
             j = float(b + 1) * (float(1 << 31) / float((key >> 33) + 1))
         return int(b)
 
@@ -92,9 +96,10 @@ def xorshift64(x):
     https://github.com/peterhinch/micropython-samples/blob/master/random/random.py
     """
     x ^= x >> 12
-    x ^= ((x << 25) & 0xffffffffffffffff)  # modulo 2**64
+    #x ^= ((x << 25) & 0xffffffffffffffff)  # modulo 2**64
+    x ^= x << 25
     x ^= x >> 27
-    return x * 0x2545f4914f6cdd1d
+    return (x * 2685821657736338717)
 
 def hashRequest(request):
     # Normalize the request parameters to ensure we're deterministic
@@ -119,7 +124,7 @@ def compactHash(string):
     return md5(string.encode('utf-8')).hexdigest()
 
 
-def carbonHash(key, hash_type):
+def carbonHash(key, hash_type, modulo=1):
     if hash_type == 'fnv1a_ch':
         big_hash = int(fnv1a(key.encode('utf-8')))
         small_hash = (big_hash >> 16) ^ (big_hash & 0xffff)
@@ -127,6 +132,9 @@ def carbonHash(key, hash_type):
         if mmh3 is None:
             raise Exception('Install "mmh3" to use this hashing function.')
         small_hash = mmh3.hash(key)
+    elif hash_type == 'jump_fnv1a_ch':
+        big_hash = int(fnv1a(key.encode('utf-8'), mode=64))
+        small_hash = jump_hash(big_hash, modulo)
     else:
         big_hash = compactHash(key)
         small_hash = int(big_hash[:4], 16)
@@ -203,73 +211,50 @@ class JumpConsistentHashRing(ConsistentHashRing):
         super().__init__(nodes, replica_count, hash_type)
 
     def compute_ring_position(self, key):
-        if sys.version_info >= (3, 0):
-            long = int
-        key64 = long(fnv1a(key.encode('utf-8'), mode=64))
-        return jump_hash(key64, len(self.ring))
+        return carbonHash(key, self.hash_type, modulo=len(self.ring))
 
     def add_node(self, key):
-        """
-        // AddNode adds a Node to the Jump Hash Ring.  Jump only operates on the
-        // number of buckets so we assume that AddNode will not be used to attempt
-        // to insert a Node in the middle of the ring as that will affect the mapping
-        // of buckets to server addresses.  This uses the instance value to define
-        // an order of the slice of Nodes.  Empty ("") instance values will be
-        // appended to the end of the slice.
-        """
-        (server, instance) = key
-        if instance is 'None': # (server, instance) = node
+        # https://github.com/grobian/carbon-c-relay/blob/v3.6/consistent-hash.c#L293
+        # https://github.com/jjneely/buckytools/blob/master/hashing/jump.go#L96
+        (server, instance) = key  # (server, instance) = node
+        if instance is 'None' or instance == '':
             self.ring.append(key)
-        # TODO - otherwise insert node to list in increasing order of instances
+        else:
+            bisect.insort(self.ring, key)
+        self.ring_len = len(self.ring)
 
     # used only in tests?
     def get_node(self, key):
-        """
-        // GetNode returns a bucket for the given key using Google's Jump Hash
-        // algorithm.
-        """
         assert self.ring
         position = self.compute_ring_position(key)
         return self.ring[position]
 
     # this used in carbonlink.py and carbon too
     def get_nodes(self, key):
-        """
-        // GetNodes returns a slice of Node objects one for each replica where the
-        // object is stored.
-        """
-        nodes = []
+        nodes = set()
         if not self.ring:
             return
         if self.nodes_len == 1:
             for node in self.nodes:
                 yield node
-        position = self.compute_ring_position(key)
-
-        # go code below - TODO
-        # Corresponding C code - https://github.com/grobian/carbon-c-relay/blob/v3.6/consistent-hash.c#L340
-        ring := make([]Node, 0)
-        ret := make([]Node, 0)
-        h := Fnv1a64([]byte(key))
-        i := len(chr.ring)
-        j := 0
-        r := chr.replicas
-
-        // We need to alter the ring as we go along, make a safe place
-        copy(ring, chr.ring)
-        for i > 0 {
-            j = Jump(h, i)
-            ret = append(ret, chr.ring[j])
-
-            if r--; r <= 0 {
-            break
-            }
-
-            // Generate a new unique hash
-            h = XorShift(h)
-
-            // Remove the previously selected bucket from our list
-            i--
-            ring[j] = ring[i]
-        }
-        return ret
+        # https://github.com/grobian/carbon-c-relay/blob/v3.6/consistent-hash.c#L340
+        # https://github.com/jjneely/buckytools/blob/master/hashing/jump.go#L130
+        hash = fnv1a(key.encode('utf-8'), mode=64)  # jump hash accepts only integers
+        ring = self.ring
+        index = self.ring_len
+        position = 0
+        r = self.replica_count
+        while index > 0:
+            position = jump_hash(hash, index)
+            nodes.add(self.ring[position])
+            yield self.ring[position]
+            r = r - 1
+            if r <= 0:
+                break
+            # use xorshift to generate a different hash for input in the jump hash again
+            hash = xorshift64(hash)
+            # remove the server we just selected, such that we can
+            # be sure the next iteration will fetch another server
+            index = index - 1
+            ring[position] = ring[index]
+        return
